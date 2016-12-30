78a42f8f0944c99e35238ed6674cd3a54db2bd94
AmitShmuel <aamits7@gmail.com>
2016-12-30 23:00:44 +0200

    add obstacles move on the screen and collision detection

--- app/src/main/java/com/example/aamit/finalproject/GameAnimationView.java
+++ app/src/main/java/com/example/aamit/finalproject/GameAnimationView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
@@ -15,30 +16,35 @@ import android.view.MotionEvent;
 public class GameAnimationView extends android.support.constraint.ConstraintLayout {
 
     private Character mainChar;
-//    private Obstacle[] obstacles;
-    float viewWidth, viewHeight;
+    private Obstacle[] obstacles;
+    private float screenWidth, screenHeight;
+    boolean touch = false;
 
     public GameAnimationView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
 
+
         mainChar = new Character();
-//        obstacles = new Obstacle[] {
-//                new Obstacle(),
-//                new Obstacle(),
-//                new Obstacle()
-//        };
-//        System.out.println("=================================="+mainChar.position.x);
+        obstacles = new Obstacle[] {
+                new Obstacle(),
+                new Obstacle(),
+                new Obstacle(),
+                new Obstacle()
+        };
+//        System.out.println("==================================");
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        viewWidth = getWidth();
-        viewHeight = getHeight();
+        screenWidth = getWidth();
+        screenHeight = getHeight();
+//        System.out.println(screenHeight);
 
         mainChar.setSize(100, 200);
-//        for (Obstacle obstacle: obstacles) {
-//        }
+        for (Obstacle obstacle: obstacles) {
+            obstacle.populate(100, 100);
+        }
 
         super.onSizeChanged(w, h, oldw, oldh);
     }
@@ -47,29 +53,33 @@ public class GameAnimationView extends android.support.constraint.ConstraintLayo
     protected void onDraw(Canvas canvas) {
 
         mainChar.draw(canvas);
-//        for (Obstacle obstacle: obstacles) {
-//            obstacle.draw(canvas);
-//        }
+        for (Obstacle obstacle: obstacles) {
+            obstacle.draw(canvas);
+        }
 
         postInvalidateOnAnimation();
+//        postInvalidateDelayed(100);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                if(mainChar.body.contains(event.getX(), event.getY()))
+                    touch = true;
+                break;
             case MotionEvent.ACTION_MOVE:
-                if(event.getY() < viewHeight/2)
-                    break;
-                mainChar.movePlayer((int) event.getX(), (int) event.getY()-100);
+                if(touch == true)
+                    mainChar.movePlayer((int) event.getX(), (int) event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                touch = false;
         }
-
         return true;
     }
 
-    class Character extends GameObject{
 
-        Paint paint = new Paint();
+    class Character extends GameObject{
 
         Character() {
             paint.setColor(Color.WHITE);
@@ -78,31 +88,66 @@ public class GameAnimationView extends android.support.constraint.ConstraintLayo
         void setSize(float width, float height) {
             objWidth = width;
             objHeight = height;
-            body.set(viewWidth/2 - objWidth/2, viewHeight*0.75f - objHeight/2, viewWidth/2 + objWidth/2, viewHeight*0.75f + objHeight/2);
+            body.set(screenWidth /2 - objWidth/2, screenHeight *0.75f - objHeight/2,
+                     screenWidth /2 + objWidth/2, screenHeight *0.75f + objHeight/2);
         }
 
         void movePlayer(int x, int y) {
-            body.set(x - body.width()/2, y - body.height()/2, x + body.width()/2, y + body.height()/2);
+            body.offsetTo(x - objWidth/2, y - objHeight);
         }
 
         @Override
         protected void draw(Canvas canvas) {
             canvas.drawOval(body, paint);
-//            System.out.println(body.toString());
         }
     }
 
-//    class Obstacle extends GameObject {
-//
-//        private final float X_SPEED = 28f;
-//
-//        Obstacle() {
-//
-//        }
-//
-//        @Override
-//        protected void draw(Canvas canvas) {
-//
-//        }
-//    }
+    class Obstacle extends GameObject {
+
+        float speed = 3f;
+
+        Obstacle() {
+            paint.setColor(Color.BLACK);
+        }
+
+        void populate(float width, float height) {
+            objWidth = width;
+            objHeight = height;
+            float rand, x, y;
+
+            // handle side edges
+            rand = (float) Math.random();
+            x = rand*screenWidth < objWidth ? rand*screenWidth+objWidth: rand*screenWidth - objWidth;
+
+            rand = (float) Math.random();
+            y = -(rand*screenHeight);
+
+            // gotta make a better gap between the obstacles
+
+            body.set(x, y - objHeight/2, x + objWidth, y + objHeight/2);
+        }
+
+        @Override
+        protected void draw(Canvas canvas) {
+//            int save = canvas.save();
+
+            canvas.drawRect(body, paint);
+            body.offsetTo(body.left, body.top + speed);
+
+            // end of screen, populate him again
+            if (body.top > screenHeight)
+                populate(objWidth, objHeight);
+
+            // collision
+            if (RectF.intersects(mainChar.body, this.body)) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+//            canvas.restoreToCount(save);
+        }
+    }
 }
--- app/src/main/java/com/example/aamit/finalproject/GameObject.java
+++ app/src/main/java/com/example/aamit/finalproject/GameObject.java
@@ -1,6 +1,7 @@
 package com.example.aamit.finalproject;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.RectF;
 
@@ -11,12 +12,10 @@ import android.graphics.RectF;
 public abstract class GameObject {
 
     protected RectF body = new RectF();
+    protected Paint paint = new Paint();
     protected float objWidth, objHeight;
     protected int color;
 
     protected abstract void draw(Canvas canvas);
 
-    protected void measure() {
-
-    }
 }
